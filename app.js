c566d38ff57a8b51e0efbdf51b71a5a2335e941a
Eric Shumway <ericshumway2@mail.weber.edu>
2022-03-16 18:23:18 -0600

    Adding validation to the question prompts

--- app.js
+++ app.js
@@ -6,13 +6,31 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
 
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your Github Username:'
+            message: 'Enter your Github Username:(Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your Github Username!');
+                    return false;
+                }
+            }
         },
 
         {
@@ -37,13 +55,31 @@ const promptProject = portfolioData => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
-            message: 'What is the name of your project?'
+            name: 'projectName',
+            message: 'What is the name of your project?(Required)',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (required)'
+            message: 'Provide a description of the project (required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a description of your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -54,7 +90,16 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your GitHub link for your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
